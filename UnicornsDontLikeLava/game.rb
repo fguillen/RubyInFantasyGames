843ef9a64de12c467daf3d081af1a8b5e09e6c33
Fernando Guillen <fguillen.mail@gmail.com>
2022-03-22 18:57:29 +0100

    UnicornsDontLikeLava: There is a rainbow on the top

--- UnicornsDontLikeLava/game.rb
+++ UnicornsDontLikeLava/game.rb
@@ -1,39 +1,37 @@
 require "fantasy" # Yeah!
 
-SCREEN_WIDTH = 500
+SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 1000
-
-# The Presentation Scene
-# on_presentation do
-#   display_top_left = HudText.new(position: Coordinates.new(0, 0))
-#   display_top_left.text = "Top-Left"
-
-#   display_top_right = HudText.new(position: Coordinates.new(SCREEN_WIDTH, 100))
-#   display_top_right.text = "Top-Right"
-#   display_top_right.alignment = "top-right"
-
-#   display_center = HudText.new(position: Coordinates.new(SCREEN_WIDTH / 2, 200))
-#   display_center.text = "Center"
-#   display_center.alignment = "center"
-#   display_center.size = "big"
-# end
 
 on_game do
   background = Background.new(image_name: "sky")
   platform_map = PlatformsMap.new
   lava = Lava.new
   unicorn = Unicorn.new
+  rainbow = Rainbow.new(position: Coordinates.new(0, platform_map.position.y - 100))
+
+  on_loop do
+    Global.camera.position.y = lava.position.y - SCREEN_HEIGHT + 150
+
+    if Global.camera.position.y < rainbow.position.y
+      Global.camera.position.y = rainbow.position.y
+    end
+  end
+
+  Global.references.rainbow = rainbow
 end
 
 class PlatformsMap < Tilemap
   def initialize
     platform_1 = Platform.new("platform_1")
     platform_2 = Platform.new("platform_2")
+    platform_moving = PlatformMoving.new("platform_moving")
 
-    super(map_name: "platforms", tiles: [platform_1, platform_2], tile_size: 72)
+    super(map_name: "platforms", tiles: [platform_1, platform_2, platform_moving], tile_width: 96, tile_height: 72)
 
     platform_1.destroy
     platform_2.destroy
+    platform_moving.destroy
 
     set_right_position
     spawn
@@ -55,6 +53,34 @@ class Platform < Actor
   end
 end
 
+class PlatformMoving < Platform
+  def initialize(image_name)
+    super(image_name)
+
+    @speed = 100
+    @direction = Coordinates.left
+    @name = "platform"
+  end
+
+  def on_collision_do(other)
+    if other.name == "platform"
+      @direction.x = -@direction.x
+    end
+  end
+
+  def on_after_move_do
+    if @position.x < 0
+      @position.x = 0
+      @direction = Coordinates.right
+    end
+
+    if @position.x > SCREEN_WIDTH - width
+      @position.x = SCREEN_WIDTH - width
+      @direction = Coordinates.left
+    end
+  end
+end
+
 class Lava < Actor
   def initialize
     super("lava")
@@ -63,13 +89,9 @@ class Lava < Actor
     @position = Coordinates.new(-10, SCREEN_HEIGHT - 100)
     @solid = true
     @layer = 3
-    @speed = 10
+    @speed = 50
 
-    on_collision do |other|
-      if other.name == "platform"
-        other.solid = false
-      end
-    end
+    @collision_with = ["unicorn"]
   end
 end
 
@@ -81,11 +103,31 @@ class Unicorn < Actor
     @layer = 2
     @solid = true
     @speed = 200
-    @jump = 200
+    @jump = 150
     @gravity = 200
     move_with_cursors(left: true, right: true, up: false, down: false, jump: true)
   end
+
+  def on_start_jumping_do
+    @image = Image.new("unicorn_jump")
+  end
+
+  def on_start_falling_do
+    @image = Image.new("unicorn")
+  end
+
+  def on_after_move_do
+    if @position.y < Global.references.rainbow.position.y + 100
+      puts "Game over"
+    end
+  end
 end
 
+class Rainbow < Actor
+  def initialize(position: )
+    super("rainbow")
+    @position = position
+  end
+end
 
 start!
