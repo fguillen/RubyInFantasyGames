6f68720edd1cb42c1ef916cd5a8c040bb8f211a2
Fernando Guillén Suárez <fguillen.mail@gmail.com>
2025-03-10 20:17:30 +0100

    Ninja game

---
+++ Ninja/game.rb
@@ -0,0 +1,36 @@
+require "fantasy"
+
+SCREEN_WIDTH = 138*3*2
+SCREEN_HEIGHT = 90*3
+
+on_game do
+  Global.background = Color.from_hex("312520")
+
+  background = Background.new(image_name: "background")
+  background.scale = 3
+  background.layer = -1
+
+  rain = Background.new(image_name: "rain")
+  rain.scale = 2
+  rain.layer = -10
+  Clock.new {
+    rain.position.y += 20
+    rain.position.y = 0 if rain.position.y > rain.height
+  }.repeat(seconds: 0.1)
+
+  floor = Background.new(image_name: "floor")
+  floor.scale = 2
+  floor.layer = 10
+  floor.repeat = :horizontal
+  floor.position = Coordinates.new(0, Global.screen_height - floor.height)
+
+  animation = Animation.new(sequence: "ninja", columns: 4, rows: 7, speed: 10, frames: [3, 7, 11, 15])
+  actor = Actor.new(animation)
+  actor.position = Coordinates.zero
+  actor.scale = 3
+  actor.layer = 0
+  actor.speed = 200
+  actor.move_with_cursors
+end
+
+start!
